ef227d32687f83ab68a45e5442ba8e5a71829cb8
Twiters300 <Tvist821@mail.ru>
2024-02-18 00:21:23 +0300

    first commit

---
+++ src/configurations/index.ts
@@ -0,0 +1,9 @@
+export const configuration = () => {
+	return {
+		port: process.env.PORT,
+		db_port: process.env.DB_PORT,
+		db_name: process.env.DB_NAME,
+		db_user: process.env.DB_USER,
+		db_password: process.env.DB_PASSWORD
+	}
+}
